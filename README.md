2b77e790a99c09eed60100d2aa7a18cc3dd76d70
oskarcp <oskarcp@gmail.com>
2022-09-21 10:07:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculadora
+Microservicio "calculadora", usando maven + spring-boot, con funcionalidades básicas de sumar y restar.
---
+++ src/main/java/com/calculadora/CalculadoraApplication.java
@@ -0,0 +1,22 @@
+package com.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Clase principal.
+ *
+ */
+
+@Configuration
+@ComponentScan(basePackages = "com.calculadora")
+@EnableAutoConfiguration
+public class CalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraApplication.class, args);
+	}
+
+}
