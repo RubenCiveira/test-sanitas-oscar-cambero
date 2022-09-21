e0510e620356e69349c76e336131d726c4a016cd
oskarcp <oskarcp@gmail.com>
2022-09-21 10:16:54 +0200

    instalación librería tracer

--- src/main/java/com/prueba/calculadora/CalculadoraApplication.java
+++ src/main/java/com/prueba/calculadora/CalculadoraApplication.java
@@ -1,4 +1,4 @@
-package com.calculadora;
+package com.prueba.calculadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
